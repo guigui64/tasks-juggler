dc352ed696c6d21340205eaf92a7ebe679bd84d3
guigui64 <guillaume.comte10@gmail.com>
2019-04-07 12:02:00 +0200

    Updates

---
+++ precache-manifest.4b3e1022b5e33933b0995e8046148d78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c4fe99b360ced4d3713",
+    "url": "/tasks-juggler/static/js/runtime~main.3ec8a7ec.js"
+  },
+  {
+    "revision": "8f24c2eaa31dcc3f63de",
+    "url": "/tasks-juggler/static/js/main.503bd4eb.chunk.js"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/tasks-juggler/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "8f24c2eaa31dcc3f63de",
+    "url": "/tasks-juggler/static/css/main.9949599a.chunk.css"
+  },
+  {
+    "revision": "aeb94535eae60e65343c46eb978160c4",
+    "url": "/tasks-juggler/index.html"
+  }
+];
