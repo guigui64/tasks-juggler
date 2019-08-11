73b7616312bf85afda8d614faa0109a75f695566
guigui64 <guillaume.comte10@gmail.com>
2019-08-12 00:18:58 +0200

    Updates

---
+++ precache-manifest.a0d6cbc20dff71aab6d11a1945bfe36f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3698acd490203c9a7217026371a4f907",
+    "url": "/tasks-juggler/index.html"
+  },
+  {
+    "revision": "00b71576d8fe092d66bb",
+    "url": "/tasks-juggler/static/css/2.2992902e.chunk.css"
+  },
+  {
+    "revision": "080ea89295d3ace4c11a",
+    "url": "/tasks-juggler/static/css/main.c415f7e4.chunk.css"
+  },
+  {
+    "revision": "00b71576d8fe092d66bb",
+    "url": "/tasks-juggler/static/js/2.d163fbb5.chunk.js"
+  },
+  {
+    "revision": "080ea89295d3ace4c11a",
+    "url": "/tasks-juggler/static/js/main.8ba9482c.chunk.js"
+  },
+  {
+    "revision": "eed3ae745b557d75fd39",
+    "url": "/tasks-juggler/static/js/runtime~main.0675a353.js"
+  },
+  {
+    "revision": "5fa1b8f25b4aa8787f70d5b6b7f0b90f",
+    "url": "/tasks-juggler/static/media/icons-16.5fa1b8f2.ttf"
+  },
+  {
+    "revision": "6fdb722c94434811f89a68eafc4531f8",
+    "url": "/tasks-juggler/static/media/icons-16.6fdb722c.eot"
+  },
+  {
+    "revision": "cd1a26696ebf17a89545a3f9067d7028",
+    "url": "/tasks-juggler/static/media/icons-16.cd1a2669.woff"
+  },
+  {
+    "revision": "14279fa7b78ac2230a015925b603a1f7",
+    "url": "/tasks-juggler/static/media/icons-20.14279fa7.eot"
+  },
+  {
+    "revision": "657723dc996cf0ae6b4c6110f5d4ddab",
+    "url": "/tasks-juggler/static/media/icons-20.657723dc.woff"
+  },
+  {
+    "revision": "90247cfdde1bd7b76d804a797d5dec56",
+    "url": "/tasks-juggler/static/media/icons-20.90247cfd.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tasks-juggler/precache-manifest.4b3e1022b5e33933b0995e8046148d78.js"
+  "/tasks-juggler/precache-manifest.a0d6cbc20dff71aab6d11a1945bfe36f.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/tasks-juggler/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tasks-juggler/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
