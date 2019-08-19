a00d85c0a08bbd32662b42e7b3ae267642df02ba
guigui64 <guillaume.comte10@gmail.com>
2019-08-19 23:35:33 +0200

    select tasks

--- src/components/task.card.tsx
+++ src/components/task.card.tsx
@@ -1,22 +1,33 @@
 import { Card, Elevation } from '@blueprintjs/core';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 type TaskCardProps = {
 	title: string;
 	desc: string;
 	duration?: number;
+	onClick: () => void;
 };
 
-const TaskCard: FC<TaskCardProps> = ({ title, desc, duration }) => (
-	<Card
-		interactive={true}
-		elevation={Elevation.TWO}
-		className='animated fadeIn fast' // TODO fadeOut when deleting
-	>
-		<h3>{title}</h3>
-		<p>{desc}</p>
-		{duration && <p>{`${duration} day(s)`}</p>}
-	</Card>
-);
+const TaskCard: FC<TaskCardProps> = ({ title, desc, duration, onClick }) => {
+	const [selected, select] = useState(false);
+	return (
+		<Card
+			interactive={true}
+			elevation={Elevation.TWO}
+			className='animated fadeIn fast' // TODO fadeOut when deleting
+			onClick={() => {
+				select(!selected);
+				onClick();
+			}}
+			style={{
+				border: `2px solid ${selected ? '#39acac' : 'rgba(0,0,0,0)'}`
+			}}
+		>
+			<h3>{title}</h3>
+			<p>{desc}</p>
+			{duration && <p>{`${duration} day(s)`}</p>}
+		</Card>
+	);
+};
 
 export default TaskCard;
--- src/components/task.group.tsx
+++ src/components/task.group.tsx
@@ -1,5 +1,5 @@
 import { Button, ButtonGroup, H3 } from '@blueprintjs/core';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { ALL_PROJECTS, NO_PROJECT } from '../constants';
 import { Task } from '../types/types';
 import TaskCard from './task.card';
@@ -10,45 +10,67 @@ type TaskGroupProps = {
 	selectedProject: number;
 };
 
-const TaskGroup: FC<TaskGroupProps> = ({ title, tasks, selectedProject }) => (
-	<div style={{ padding: '50px 50px 0 50px' }}>
-		<div style={{ display: 'inline' }}>
-			<H3 style={{ display: 'inline-block', marginRight: '10px' }}>{title}</H3>
-			<ButtonGroup>
-				{/* TODO add and delete task */}
-				<Button
-					text='Add task'
-					icon='add'
-					onClick={() => console.log('TODO add task')}
-				/>
-				<Button
-					text='Delete tasks'
-					icon='trash'
-					onClick={() => console.log('TODO delete tasks')}
-				/>
-			</ButtonGroup>
+const TaskGroup: FC<TaskGroupProps> = ({ title, tasks, selectedProject }) => {
+	const [selectedIds, setSelectedIds] = useState([] as number[]);
+	const selectId = (id: number) => {
+		let newSelectedIds = [...selectedIds]; // shallow copy
+		if (newSelectedIds.includes(id)) {
+			newSelectedIds = selectedIds.filter(s => s !== id);
+		} else {
+			newSelectedIds.push(id);
+		}
+		setSelectedIds(newSelectedIds);
+		console.log(newSelectedIds);
+	};
+	return (
+		<div style={{ padding: '50px 50px 0 50px' }}>
+			<div style={{ display: 'inline' }}>
+				<H3 style={{ display: 'inline-block', marginRight: '10px' }}>
+					{title}
+				</H3>
+				<ButtonGroup>
+					{/* TODO add and delete task */}
+					<Button
+						text='Add task'
+						icon='add'
+						onClick={() => console.log('TODO add task')}
+					/>
+					<Button
+						text={'Delete task' + (selectedIds.length !== 1 ? 's' : '')}
+						icon='trash'
+						onClick={() => console.log('TODO delete tasks')}
+						disabled={selectedIds.length === 0}
+					/>
+				</ButtonGroup>
+			</div>
+			<div
+				style={{
+					marginTop: '10px',
+					display: 'grid',
+					gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))',
+					gridGap: '10px'
+				}}
+			>
+				{tasks
+					.filter(
+						t =>
+							t.projectId === selectedProject ||
+							(selectedProject === ALL_PROJECTS && t.projectId !== NO_PROJECT)
+					)
+					.map(task => {
+						let { title, desc, duration, id } = task;
+						return (
+							<TaskCard
+								{...{ title, desc, duration }}
+								key={id}
+								onClick={() => selectId(id)}
+							/>
+						);
+					})}
+			</div>
 		</div>
-		<div
-			style={{
-				marginTop: '10px',
-				display: 'grid',
-				gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))',
-				gridGap: '10px'
-			}}
-		>
-			{tasks
-				.filter(
-					t =>
-						t.projectId === selectedProject ||
-						(selectedProject === ALL_PROJECTS && t.projectId !== NO_PROJECT)
-				)
-				.map(task => {
-					let { title, desc, duration, id } = task;
-					return <TaskCard {...{ title, desc, duration }} key={id} />;
-				})}
-		</div>
-	</div>
-);
+	);
+};
 
 export default TaskGroup;
 
